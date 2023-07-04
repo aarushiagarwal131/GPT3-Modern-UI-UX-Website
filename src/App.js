3c8ae4169e4dabc3e3e0f33434a171954c9827c0
aarushiagarwal131 <100944778+aarushiagarwal131@users.noreply.github.com>
2023-07-05 00:07:06 +0530

    my first commit to react website

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { CTA, Brand, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
